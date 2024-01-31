e76f8b0e516fb6c3747488dc9c6486187d8b687b
RikersMax <larionov01996@gmail.com>
2024-01-31 20:30:40 +0400

    mini-bar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,22 +1,27 @@
 class ProductsController < ApplicationController
+  before_action(:product_find, only: %i[edit show destroy update])
+  before_action(:target_select, only: %i[edit show new update])
 
   def index
-    @products = Product.all
+    @products = Product.order(id: :desc)
+  end
+
+  def show
+
   end
 
   def new
-    @targets = Target.all.map{|t| [t.name, t.id]}
     @product = Product.new
   end
 
   def create
-    @product = Product.new(set_params)
+    @product = Product.new(product_params)
 
-    if @product.valid? then
-      @product.save
+    if @product.save then
+      flash[:notice] = 'Изделие успешно созданно'
       redirect_to(products_path)
     else
-      @targets = Target.all.map{|t| [t.name, t.id]}
+      flash.now[:danger] = 'Ошибка ввода'
       render(:new)
     end
   end
@@ -26,13 +31,35 @@ class ProductsController < ApplicationController
   end
 
   def update
+    if @product.update(product_params)  then
+      redirect_to(products_path)
+    else
+      flash.now[:danger] = 'test'
+      render(:edit)
+    end
+    #render(plain: params.to_yaml)
+  end
 
+  def destroy
+    flash[:info] = "#{@product.name} удален"
+    @product.destroy
+    redirect_to(root_path)
+
+    #render(plain: params.to_yaml)
   end
 
 
   private
 
-  def set_params
+  def target_select
+    @targets = Target.all.map{|t| [t.name, t.id]}
+  end
+
+  def product_find
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
     params.require(:product).permit(:name, :ident_number, :target_id, :description)
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,37 @@ module ApplicationHelper
     form_object.object.persisted? ? if_exists : if_not_exists
   end
 
-  def correct_date(object)
+  def currect_date(object)
     object.strftime('%Y-%m-%d')
   end
 
-  def corrent_title
+  def current_title(page_title=nil)
+    base_title = 'CS'
+    if page_title.present?
+      return(" #{base_title} | #{page_title}")
+    else
+      return(base_title)
+    end
+  end
 
+  def nav_tab(title, url, options={})
+    current_page = options.delete(:current_page)
+    class_css = current_page == title ? 'secondary-link' : 'color-link'
+
+    if options[:class] then
+      options[:class] = options[:class] + ' ' + class_css
+    else
+      options[:class] = class_css
+    end
+
+    return(link_to(title, url, options))
+  end
+
+  def currently_at(current_page = '')
+    render(partial: 'shared/menu', locals: {current_page: current_page})
+  end
+
+  def currently_at_prod(current_page = '')
+    render(partial: 'shared/mini_bar_prod', locals: {current_page: current_page})
   end
 end
