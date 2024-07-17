8f26131fc6c07090633e5d411dccc6ac0357e86f
RikersMax <larionov01996@gmail.com>
2024-07-17 23:59:06 +0400

    footer edit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,7 @@ class OrdersController < ApplicationController
   end
 
   def arrival_of_goods # приход
-    role_check([3, 4, 5])
+    role_check([2, 3, 4, 5])
     @hidden_field = '1'
     @name_form = 'Приход'
 
@@ -24,7 +24,7 @@ class OrdersController < ApplicationController
   end
 
   def consumption_of_goods # расод
-    role_check([3, 4, 5])
+    role_check([2, 3, 4, 5])
     @hidden_field = '2'
     @name_form = 'Расход'
 
@@ -33,7 +33,7 @@ class OrdersController < ApplicationController
   end
 
   def create_arrival_of_goods # приход
-    role_check([3, 4, 5])
+    role_check([2, 3, 4, 5])
     @order = Order.new(order_params)
     # render(plain: params[:order])
 
@@ -52,7 +52,7 @@ class OrdersController < ApplicationController
   end
 
   def create_consumption_of_goods # расод
-    role_check([3, 4, 5])
+    role_check([2, 3, 4, 5])
     @order = Order.new(order_params)
     # render(plain: params[:order])
 
@@ -71,13 +71,13 @@ class OrdersController < ApplicationController
   end
 
   def edit
-    role_check([3, 4, 5])
+    role_check([2, 3, 4, 5])
     @name_form = 'Изменить'
     @hidden_field = @order.movement_id.to_s
   end
 
   def update
-    role_check([3, 4, 5])
+    role_check([2, 3, 4, 5])
     new_quantity = update_order_product(@order, order_params)
 
     if @order.update(order_params)
@@ -92,7 +92,7 @@ class OrdersController < ApplicationController
   end
 
   def destroy
-    role_check([3, 4, 5])
+    role_check([2, 3, 4, 5])
     update_product_delete_order(@order)
 
     @order.destroy
